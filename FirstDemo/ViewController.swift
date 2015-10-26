70d85b2c8000a29ce61849de35daa0caeb0f31a3
Edward Chiang <edwardubuntu@gmail.com>
2015-10-26 17:31:57 +0800

    Initial Commit

---
+++ FirstDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstDemo
+//
+//  Created by Edward Chiang on 10/26/15.
+//  Copyright © 2015 Soleil Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
